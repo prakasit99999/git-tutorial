07b0693232300768484e779bcd0a0cb14681e9ca
p19237 <85072379+prakasit99999@users.noreply.github.com>
2024-07-08 02:56:10 +0700

    version breach mahankorlis

---
+++ aboutme.php
@@ -0,0 +1,4 @@
+<?php
+echo '<h1>About Me</h1>';
+echo '<p>My name is Alex and I am a web developer.</p>';
+?>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php echo "My repository" ?>
-<?php echo "My id is 6611860027" ?>
-<?php echo "My FustName is Prakasit Somaung" ?>
-<?php echo "Age is 22" ?>
